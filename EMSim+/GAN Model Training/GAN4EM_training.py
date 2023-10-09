3d3bf7e8a02c87d0a467f3b0250ad07445b8181f
Ya_Gao <96329131+Ya0824@users.noreply.github.com>
2023-10-09 11:37:14 +0800

    Update GAN4EM_training.py

--- EMSim+/GAN Model Training/GAN4EM_training.py
+++ EMSim+/GAN Model Training/GAN4EM_training.py
@@ -5,8 +5,8 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import tensorflow as tf
 
-from tensorflow.keras.layers import Input, Dense, Flatten, Conv2D, MaxPooling2D, UpSampling2D, Conv2DTranspose, Concatenate
-from tensorflow.keras import Model, regularizers
+from tensorflow.keras.layers import Input
+from tensorflow.keras import Model
 import numpy as np
 import matplotlib.pyplot as plt
 from time import time
@@ -17,6 +17,7 @@ from discriminator import Discriminator
 import shutil
 from Logger import Logger
 import sys
+import argparse
 
 
 def getToolLogo():
@@ -33,18 +34,9 @@ def getToolLogo():
 	]
 	return '\n'.join(logo) + '\n'
 	
-#保存运行过程的控制台信息
+
 sys.stdout = Logger("Default.txt", sys.stdout)
 
-num_probe_x_tiles = 48
-num_probe_y_tiles = 48
-time_steps = 20
-percent_valid_split = 0.9  # 10 percent of training points is used for validation during training
-learning_rate_val = 0.0005
-num_input_stimuli = 750
-decay_rate_val = 0.98
-epochs = 100
-batch_size = 64
 
 class CGAN():
     def __init__(self):
@@ -156,83 +148,6 @@ class CGAN():
         print('val_best_loss:', best_loss, 'best_epoch', best_epoch)
 
 
-    def EM_prediction(self, normalization_data, current_map_data, grid_map_data, em_map_data, model_path):
-        current_map = np.load(current_map_data)
-        grid_map = np.load(grid_map_data)
-        em_map = np.load(em_map_data)
-
-        data_runs = np.shape(current_map)[0]
-
-        self.Generator.load_weights(model_path)
-        max_x = normalization_data[0]
-        min_x = normalization_data[1]
-        max_g = normalization_data[2]
-        min_g = normalization_data[3]
-        max_t = normalization_data[4]
-        min_t = normalization_data[5]
-        max_y = normalization_data[6]
-        min_y = normalization_data[7]
-        Writer = animation.writers['html']
-        writer = Writer(fps=5, metadata=dict(artist='Me'), bitrate=1800)
-        im = []
-        figs = []
-        anis = []
-        num_im = data_runs
-        predicted_video = np.zeros((num_im, time_steps, num_probe_x_tiles, num_probe_y_tiles))
-        err_video = np.zeros((num_im, time_steps, num_probe_x_tiles, num_probe_y_tiles))
-        for im_num in range(num_im):
-            def updatefig1(frame, im_num):
-                im[im_num*3].set_array(em_map[im_num, frame, ...])
-                im[im_num*3+1].set_array(predicted_video[im_num, frame, ...])
-    
-            def updatefig2(frame, im_num):
-                im[im_num*3+2].set_array(err_video[im_num, frame, ...])
-    
-            for frame in range(time_steps):
-                data_pt = np.array([[frame]])/max_t
-                in_data = current_map[im_num:im_num+1, frame, ..., np.newaxis]
-                in_data = (in_data-min_x)/(max_x-min_x)
-                g_data = grid_map[np.newaxis, ..., np.newaxis]
-                g_data = (g_data - min_g) / (max_g - min_g)
-                in_data = np.concatenate((in_data, g_data), axis=-1)
-                predicted = self.Generator.predict((in_data, data_pt))
-                predicted_video[im_num, frame, ...] = (np.squeeze(predicted)*(max_y-min_y))+min_y
-                #predicted_video[im_num, frame, ...] = np.squeeze(predicted)
-                err_video[im_num, frame, ...] = predicted_video[im_num, frame, ...] - em_map[im_num, frame, ...]
-            print("Writing result of test: ", im_num)
-            test_data_num = im_num
-            fig, axes = plt.subplots(1, 2)
-            max_val = np.max((np.max(em_map[im_num, ...]), np.max(predicted_video[im_num, ...])))
-            max_err = np.max(err_video[im_num, ...])
-            min_err = np.min(err_video[im_num, ...])
-            im.append(axes[0].imshow(em_map[im_num, 0], vmin=0, vmax=max_val))
-            im.append(axes[1].imshow(predicted_video[im_num, 0, ...], vmin=0, vmax=max_val))
-            fig.colorbar(im[3*im_num+1], ax=axes.ravel().tolist())
-    
-            anis.append(animation.FuncAnimation(fig, updatefig1, frames=range(time_steps), fargs=(im_num,), interval=150))
-            anis[2*im_num].save("output_plots/run_contours_%d.html" % test_data_num, writer=writer)
-            plt.close()
-            fig2 = plt.figure()
-            im.append(plt.imshow(err_video[im_num, 0, ...], vmin=min_err, vmax=max_err))
-            plt.colorbar()
-            anis.append(animation.FuncAnimation(fig2, updatefig2, frames=range(time_steps), fargs=(im_num,), interval=150))
-            anis[2*im_num+1].save("output_plots/err_contours_%d.html" % test_data_num, writer=writer)
-            plt.close()
-            dir = os.path.join("./", "output_plots", "output_plots")
-            if not os.path.exists(dir):
-                os.mkdir(dir)
-                os.rename("output_plots/run_contours_%d_frames" % test_data_num, "output_plots/output_plots/run_contours_%d_frames" % test_data_num )
-                os.rename("output_plots/err_contours_%d_frames" % test_data_num, "output_plots/output_plots/err_contours_%d_frames" % test_data_num )
-            else:
-                if os.path.exists("output_plots/output_plots/run_contours_%d_frames" % test_data_num) and os.path.exists("output_plots/output_plots/err_contours_%d_frames" % test_data_num):
-                    shutil.rmtree("output_plots/output_plots/run_contours_%d_frames" % test_data_num)
-                    shutil.rmtree("output_plots/output_plots/err_contours_%d_frames" % test_data_num)
-                os.rename("output_plots/run_contours_%d_frames" % test_data_num, "output_plots/output_plots/run_contours_%d_frames" % test_data_num)
-                os.rename("output_plots/err_contours_%d_frames" % test_data_num, "output_plots/output_plots/err_contours_%d_frames" % test_data_num)
-
-        np.save('Training_EM_prediction.npy', predicted_video)
-        np.save('Err_EM_prediction.npy', err_video)
-
     def read_data(self, current_map_data, grid_map_data, em_map_data):
         current_map = np.load(current_map_data)
         grid_map = np.load(grid_map_data)
@@ -289,14 +204,50 @@ class CGAN():
 if __name__ == '__main__':
     start_time = time() # obtain the starting time of code.
 
-    # Model save path
-    trained_model_path = './weights/generator_trained_weights.h5'
+    parser = argparse.ArgumentParser()
 
-    # load training dataset
-    training_current_map = 'current_map_train.npy'
-    training_grid_map = 'power_grid_map.npy'
-    training_em_map = 'em_map_train.npy'
- 
+    parser.add_argument("--num_probe_x_tiles", type=int, default=48,
+                        help=" number of point grid in x axial direction ")
+    parser.add_argument("--num_probe_y_tiles", type=int, default=48,
+                        help=" number of point grid in y axial direction ")
+    parser.add_argument("--time_steps", type=int, default=20,
+                        help=" simulation time for each EM trace ")
+    parser.add_argument("--percent_valid_split", type=int, default=0.9,
+                        help=" 10% of training points is used for validation during training ")
+    parser.add_argument("--learning_rate_val", type=int, default=0.0005,
+                       help=" learning rate decays exponentially from 0.0005 ")
+    parser.add_argument("--num_input_stimuli", type=int, default=750,
+                        help=" number of plaintexts ")
+    parser.add_argument("--decay_rate_val", type=int, default=0.98,
+                        help="  learning rate with the discount factor 0.98 ")
+    parser.add_argument("--epochs", type=int, default=100,
+                        help="  number of times the model worked on the entire training dataset ")
+    parser.add_argument("--batch_size", type=int, default= 64,
+                        help="  number of samples processed per training epoch ")
+    parser.add_argument("--trained_model_path", type=str, default="./weights/generator_trained_weights.h5",
+                        help=" save the trained weight parameters ")
+    parser.add_argument("--training_current_map", type=str, default="./current_map_train.npy",
+                        help=" input cell current map for model training")
+    parser.add_argument("--training_grid_map", type=str, default="./power_grid_map.npy",
+                        help=" input power grid map for model training ")
+    parser.add_argument("--training_em_map", type=str, default="./em_map_train.npy",
+                        help=" input EM map for model training ")
+
+    args = parser.parse_args()
+
+    num_probe_x_tiles = args.num_probe_x_tiles
+    num_probe_y_tiles = args.num_probe_y_tiles
+    time_steps = args.time_steps
+    percent_valid_split = args.percent_valid_split
+    learning_rate_val = args.learning_rate_val
+    num_input_stimuli = args.num_input_stimuli
+    decay_rate_val = args.decay_rate_val
+    epochs = args.epochs
+    batch_size = args.batch_size
+    trained_model_path = args.trained_model_path
+    training_current_map = args.training_current_map
+    training_grid_map = args.training_grid_map
+    training_em_map = args.training_em_map
 
     print(getToolLogo())
     print('Step 1: Build the cGAN model')
@@ -312,13 +263,3 @@ if __name__ == '__main__':
 
     model = cgan.train_model(x_data, t_data, y_data, trained_model_path)
 
-    #print('Step 5: Evaluate the pre-trained cGAN model')
-
-    #trained_model_path = './weights/generator_pretrained_weights.h5'
-
-    #cgan.EM_prediction(normalization_data, training_current_map, training_current_map, training_current_map, trained_model_path)
-
-    #print("\nTotal running time of pre-training stage: --- %s seconds ---\n" % (time() - start_time))
-
-#print(file = outputfile)
-#outputfile.close() # close后才能看到写入的数据
